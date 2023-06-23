7b4c932845ab5d0e8cf56b7c7dcf47a0fec0c63f
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-06-23 21:42:52 +0300

    auth

--- src/components/WelcomePageComponent/WelcomePageComponent.styled.jsx
+++ src/components/WelcomePageComponent/WelcomePageComponent.styled.jsx
@@ -18,10 +18,10 @@ export const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-   position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%) }
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) }
 `;
 
 export const LogoWrap = styled.div`
