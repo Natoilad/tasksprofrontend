77146e4a2b11f575d220539516833c355f0d61a4
IvanDemchyk <ivandem4ik@gmail.com>
2023-06-23 22:15:42 +0300

    sidebar start

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+export const Sidebar = () => {
+  return (
+    <div>
+      <div></div>
+      <p>My boards</p>
+      <button></button>
+      <ul></ul>
+      <div></div>
+      <button></button>
+    </div>
+  );
+};
