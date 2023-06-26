3e92c1019c8583494145e5c1c76b7db189d5b9d8
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-06-26 21:24:28 +0300

    fix logout

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,9 @@
 import HeaderDashboard from 'components/HeaderDashboard/HeaderDashboard';
 import MainDashboard from 'components/MainDashboard/MainDashboard';
 // import BoardIsNotYet from 'components/BoardIsNotYet/BoardIsNotYet';
-import { authLogout } from 'redux/auth/auth-operations';
-import { useDispatch } from 'react-redux';
 // import { useState } from 'react';
 // import { Conteiner } from 'components/Board/Board.styled';
 const Board = () => {
-  const dispatch = useDispatch();
   // const [isItBord, setIsItBord] = useState(true);
   return (
     // <Conteiner>
@@ -14,11 +11,7 @@ const Board = () => {
       <HeaderDashboard title="Name of board" />
       {/* {isItBord ? */}
       <MainDashboard />
-      {/* // } */}
-      <button type="button" onClick={() => dispatch(authLogout())}>
-        <b>Logout</b>
-      </button>
-      ;
+      {/* // } */};
     </>
 
     //  </Conteiner>
--- src/components/Sidebar/LogOutBtn/LogOutBtn.jsx
+++ src/components/Sidebar/LogOutBtn/LogOutBtn.jsx
@@ -1,9 +1,12 @@
+import { authLogout } from 'redux/auth/auth-operations';
 import icon from '../../../images/sprite.svg';
 import { LogOutBox, LogOutIcon } from './LogOutBtn.styled';
+import { useDispatch } from 'react-redux';
 
 export const LogOutBtn = () => {
+  const dispatch = useDispatch();
   return (
-    <LogOutBox>
+    <LogOutBox onClick={() => dispatch(authLogout())}>
       <LogOutIcon>
         <use href={icon + '#icon-logout'}></use>
       </LogOutIcon>
