24699c3d9f9fa2c3cf0cc0d7a7e93e53fe78993f
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-07-03 03:16:55 +0300

    fix

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -3,24 +3,24 @@ import React, { useContext } from 'react';
 
 import { ModalContext } from '../../contexts';
 
-export const ContainerModal = (props) => {
+export const ContainerModal = props => {
   const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    };
-    
-    const { children, title } = props;
+  };
 
-    const { openModal, closeModal } = useContext(ModalContext);
+  const { children, title } = props;
+
+  const { openModal, closeModal } = useContext(ModalContext);
 
   return (
     <Modal open={openModal} onClose={closeModal}>
-          <div style={style}>
-              <h2>{title}</h2>
-            {children}
-          </div>
+      <div style={style}>
+        <h2>{title}</h2>
+        {children}
+      </div>
     </Modal>
   );
-}
+};
