714369d9353457928ada993ada355d321f46d4f2
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-07-06 03:08:24 +0300

    Update EditProfileModal

--- src/components/Modals/EditProfileModal/EditProfileModal.jsx
+++ src/components/Modals/EditProfileModal/EditProfileModal.jsx
@@ -1,67 +1,53 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Formik } from 'formik';
-import { Form as FormWrap, Field as Input } from 'formik';
 import * as Yup from 'yup';
 
-import { Close as CloseIcon } from '@mui/icons-material';
-import AddIcon from '@mui/icons-material/Add';
-import { IconButton } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { useAuth } from 'hooks/authHooks';
 
-import { DropzoneBox } from 'components/DropzoneBox/DropzoneBox';
-import styles from './EditProfileModal.module.css';
-
-import userImg from '../../..//images/unknown-user.png';
-
-import {
-  Wrap,
-  // Button,
-  Title,
-} from './EditProfileModal.styled';
-import { useDispatch } from 'react-redux';
 import {
   authUpdateUser,
   authUpdateUserAvatar,
 } from 'redux/auth/auth-operations';
-import { useAuth } from 'hooks/authHooks';
 
-const Form = styled(FormWrap)({
-  display: ' flex',
-  flexDirection: ' column',
-  rowGap: '14px',
-  alignItems: 'center',
-});
+import icon from '../../../images/sprite.svg';
 
-const StyledIconButton = styled(IconButton)({
-  color: 'white',
-});
-const Field = styled(Input)({
-  width: '100%',
-  paddingLeft: '10px',
-  height: ' 49px',
-  borderRadius: '8px',
-  border: '1px solid #bedbb0',
-  opacity: '0.4000000059604645',
-  background: '#1f1f1f',
-  boxShadow: '0px 4px 16px 0px rgba(22, 22, 22, 0.08)',
-  color: '#fff',
+import { DropzoneBox } from 'components/DropzoneBox/DropzoneBox';
+import {
+  Wrap,
+  EditForm,
+  Title,
+  CloseBtn,
+  CloseIcon,
+  AvatarBox,
+  AddBtnBox,
+  FormInput,
+  PasswordWrap,
+  EyeButton,
+  ErrorBox,
+  SendBtn,
+} from './EditProfileModal.styled';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is Required'),
+  email: Yup.string().email().required('Email is Required'),
+  password: Yup.string()
+    .min(8, 'Too Short!')
+    .max(64, 'Too Long!')
+    .required('Password is Required'),
 });
 
 export const EditProfileModal = ({ handleClose }) => {
   const dispatch = useDispatch();
   const { user: currentUser } = useAuth();
   const [uploadImg, setUploadImg] = useState(null);
+  const [passwordShown, setPasswordShown] = useState(false);
   const [img, setImg] = useState(currentUser.avatarURL || '');
 
-  const schema = Yup.object().shape({
-    name: Yup.string().required('Name is Required'),
-    email: Yup.string().email().required('Email is Required'),
-    password: Yup.string()
-      .min(8, 'Too Short!')
-      .max(64, 'Too Long!')
-      .required('Password is Required'),
-  });
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
 
   const onFormSubmit = (values, { resetForm }) => {
     const user = {
@@ -87,76 +73,76 @@ export const EditProfileModal = ({ handleClose }) => {
   return (
     <Wrap>
       <Title>Edit profile</Title>
-      <div className={styles.btn_close}>
-        <StyledIconButton aria-label="close" onClick={handleClose}>
-          <CloseIcon />
-        </StyledIconButton>
-      </div>
+      <CloseBtn aria-label="close" onClick={handleClose}>
+        <CloseIcon>
+          <use href={icon + '#icon-close'}></use>
+        </CloseIcon>
+      </CloseBtn>
 
-      <div>
-        <Formik
-          initialValues={{
-            name: currentUser.name,
-            email: currentUser.email,
-            password: '',
-          }}
-          validationSchema={schema}
-          onSubmit={onFormSubmit}
-        >
-          {({ errors, touched }) => (
-            <Form autoComplete="off">
-              <div className={styles.box_icon_user}>
-                <div className={styles.icon_dropzone}>
-                  {img ? (
-                    <img
-                      style={{ borderRadius: '4px' }}
-                      className={styles.icon_user}
-                      src={img}
-                      alt="user"
-                    />
-                  ) : (
-                    <img
-                      className={styles.icon_user}
-                      src={img ? img : userImg}
-                      alt="unknown user img"
-                    />
-                  )}
-                  <DropzoneBox setUploadImg={setUploadImg} setImg={setImg} />
-                  <div className={styles.box_add}>
-                    <AddIcon width="10px" height="10px" />
-                  </div>
-                </div>
+      <Formik
+        initialValues={{
+          name: currentUser.name,
+          email: currentUser.email,
+          password: '',
+        }}
+        validationSchema={schema}
+        onSubmit={onFormSubmit}
+      >
+        {({ errors, touched }) => (
+          <EditForm autoComplete="off">
+            <AvatarBox>
+              <div className="icon_dropzone">
+                {img ? (
+                  <img className="icon_user" src={img} alt="User avatar" />
+                ) : (
+                  <svg className="icon_user">
+                    <use href={icon + '#icon-user-avatar'}></use>
+                  </svg>
+                )}
+                <DropzoneBox setUploadImg={setUploadImg} setImg={setImg} />
+                <AddBtnBox>
+                  <svg width="10" height="10">
+                    <use href={icon + '#icon-plus-fill'}></use>
+                  </svg>
+                </AddBtnBox>
               </div>
-              <Field
-                name="name"
-                type="text"
-                autoComplete="off"
-                placeholder="Enter your Name"
-              />
-              {errors.name && touched.name ? (
-                <div className={styles.error}>{errors.name}</div>
-              ) : null}
-              <Field name="email" type="email" placeholder="Enter your email" />
-              {errors.email && touched.email ? (
-                <div className={styles.error}>{errors.email}</div>
-              ) : null}
+            </AvatarBox>
 
-              <Field
+            <FormInput
+              name="name"
+              type="text"
+              autoComplete="off"
+              placeholder="Enter your Name"
+            />
+            {errors.name ? <ErrorBox>{errors.name}</ErrorBox> : null}
+            <FormInput
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+            {errors.email ? <ErrorBox>{errors.email}</ErrorBox> : null}
+            <PasswordWrap>
+              <FormInput
                 name="password"
-                type="password"
+                type={passwordShown ? 'text' : 'password'}
                 placeholder="Create a password"
               />
-              {errors.password && touched.password ? (
-                <div className={styles.error}>{errors.password}</div>
-              ) : null}
+              <EyeButton onClick={togglePassword} type="button">
+                <svg width="18" height="18">
+                  <use href={icon + '#icon-eye'}></use>
+                </svg>
+              </EyeButton>
+            </PasswordWrap>
+            {errors.password && touched.password ? (
+              <ErrorBox>
+                <p>{errors.password}</p>
+              </ErrorBox>
+            ) : null}
 
-              <button className={styles.btn_submit} type="submit">
-                Send
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
+            <SendBtn type="submit">Send</SendBtn>
+          </EditForm>
+        )}
+      </Formik>
     </Wrap>
   );
 };
--- src/components/Modals/EditProfileModal/EditProfileModal.styled.js
+++ src/components/Modals/EditProfileModal/EditProfileModal.styled.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
+import { Form, Field } from 'formik';
 
 export const Wrap = styled.div`
   position: relative;
-  width: 400px;
   padding: 24px;
   border-radius: 8px;
   background: ${({ theme }) => theme.colors.secondaryBg};
-`;
 
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 24px;
+  @media screen and (min-width: 375px) {
+    width: 335px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 400px;
+  }
 `;
 
 export const Title = styled.h4`
@@ -20,24 +21,9 @@ export const Title = styled.h4`
   font-size: 18px;
   font-weight: 500;
   letter-spacing: -0.36px;
+  line-height: 1.5;
   margin-right: auto;
-`;
-
-export const Button = styled.button`
-  width: 100%;
-  padding: 10px 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  border-radius: 8px;
-  background: ${({ theme }) => theme.colors.accent};
-  color: ${({ theme }) => theme.colors.btnText};
-  text-align: center;
-  font-size: 14px;
-  font-weight: 500;
-  font-family: 'Poppins', sans-serif;
-  letter-spacing: -0.28px;
+  margin-bottom: 24px;
 `;
 
 export const CloseBtn = styled.button`
@@ -54,4 +40,155 @@ export const CloseIcon = styled.svg`
   width: 18px;
   height: 18px;
   stroke: ${({ theme }) => theme.colors.primaryText};
+`;
+
+export const EditForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const AvatarBox = styled.div`
+  position: relative;
+  width: 68px;
+  height: 68px;
+  margin-bottom: 25px;
+
+  cursor: pointer;
+
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.colors.pageBg};
+  fill: ${({ theme }) => theme.colors.userIconFill};
+
+  & .icon_dropzone {
+    width: 68px;
+    height: 68px;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+
+  & .icon_user {
+    width: 68px;
+    height: 68px;
+    border-radius: 8px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+export const AddBtnBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  z-index: 1;
+  position: absolute;
+  bottom: -12px;
+  left: 50%;
+  cursor: pointer;
+  pointer-events: none;
+
+  background: ${({ theme }) => theme.colors.additionalAccent};
+
+  width: 24px;
+  height: 24px;
+  border-radius: 4px;
+  transform: translate(-50%);
+
+  fill: black;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.btnFocus};
+  }
+`;
+
+export const FormInput = styled(Field)`
+  width: 100%;
+  height: 49px;
+  padding: 14px 0 14px 18px;
+
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.colors.inputBorder};
+  box-shadow: 0px 4px 16px 0px ${({ theme }) => theme.colors.inputShadow};
+  opacity: 0.4000000059604645;
+
+  background: ${({ theme }) => theme.colors.tertiaryBg};
+  color: ${({ theme }) => theme.colors.primaryText};
+
+  &:focus {
+    outline: 0;
+    opacity: 1;
+  }
+
+  & + & {
+    margin-top: 14px;
+  }
+`;
+
+export const PasswordWrap = styled.div`
+  position: relative;
+  margin-top: 14px;
+  width: 100%;
+`;
+
+export const EyeButton = styled.button`
+  position: absolute;
+  top: 14px;
+  right: 12px;
+
+  background: inherit;
+  border: none;
+  cursor: pointer;
+  display: flex;
+
+  justify-content: center;
+
+  opacity: 0.4000000059604645;
+  stroke: ${({ theme }) => theme.colors.primaryText};
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const SendBtn = styled.button`
+  width: 100%;
+
+  padding-top: 14px;
+  padding-bottom: 14px;
+
+  margin-top: 24px;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+
+  border: none;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.accent};
+  color: ${({ theme }) => theme.colors.btnText};
+
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: -0.02;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.btnFocus};
+  }
+`;
+
+export const ErrorBox = styled.div`
+  position: relative;
+  padding: 2px 0px;
+
+  font-size: 10px;
+
+  color: red;
 `;
